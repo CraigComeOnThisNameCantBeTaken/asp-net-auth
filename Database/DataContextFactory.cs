c42437dc140c47f72effede4cc34614ff96271ff
Craig Freeman <freema48@gmail.com>
2020-03-24 12:48:21 +0000

    Setup identity tables

---
+++ Database/DataContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        protected IConfiguration configuration;
+
+        public DataContextFactory()
+        {
+
+        }
+
+        public DataContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<DataContext>();
+            builder.UseSqlServer("Server=localhost;Database=AspNetAuth;Trusted_Connection=True");
+            return new DataContext(builder.Options);
+        }
+    }
+}
---
+++ asp-net-auth/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
